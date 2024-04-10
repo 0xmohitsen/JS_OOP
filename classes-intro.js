ff065af4b56c645ddebf27772476cdb828c907d0
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-11 00:04:50 +0530

    Added comments for better understanding

--- classes-intro.js
+++ classes-intro.js
@@ -1,34 +1,45 @@
+// class initalization and implementaion
 class Product {
+    // constructor declaration
     constructor(name, price) {
-        this.name = name;
-        this.price = price;
+        // it is used to create objects
+        // it gets called first at the time of object creation
+        this.name = name; // data member
+        this.price = price; // data member
     }
 
-    getPrice() {
+    // member function
+    getPrice() { // getter member function
+        // use this keyword to access data member inside member functions
         return this.price;
     }
 
-    setPrice(price) {
+    setPrice(price) { // setter member function
+        // use this keyword to access data member inside member functions
         this.price = price;
     }
 
-    getName() {
+    getName() { // getter member function
+        // use this keyword to access data member inside member functions
         return this.name;
     }
 
-    setName(name) {
+    setName(name) { // setter member function
+        // use this keyword to access data member inside member functions
         this.name = name;
     }
 
-    displayProduct() {
+    displayProduct() { 
         console.log(`Product Name : ${this.name}\nProduct Price : ${this.price}`);
     }
 }
 
+// object creation
 const product1 = new Product("Iphone", 120000);
 
 console.log(product1);
 
+// to access the member functions use dot(.) operator
 product1.displayProduct(); 
 
 const price = product1.getPrice();
